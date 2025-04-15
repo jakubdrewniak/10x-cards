4dbbed262533b2d8698c6c074d72d47ad86fb606
jakubdrewniak <drewniak.jakub@@gmail.com>
2025-04-15 21:45:35 +0200

    View fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import path from 'path';
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -9,10 +10,19 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  experimental: {
+    session: true
+  },
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: [{
+        find: '@',
+        replacement: path.resolve('./src')
+      }]
+    }
   },
   adapter: node({
     mode: "standalone",
